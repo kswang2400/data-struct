1f0b67672d366a353a0836fcd34ebf6425ac59bf
Daniel Ng <dvng13@gmail.com>
2015-07-17 10:34:34 -0700

    add doubly linked list rspec

--- Ruby/lib/doubly_linked_list.rb
+++ Ruby/lib/doubly_linked_list.rb
@@ -8,7 +8,7 @@ class LinkedList
   end
 
   def pop
-    last_link = @last_sentinel.last
+    last_link = @last_sentinel.prev
     val = last_link.val
     @last_sentinel.prev = last_link.prev
 
@@ -17,16 +17,25 @@ class LinkedList
 
   def push(val)
     link = Link.new(val)
-    @last_sentinel.prev.next, link.prev = link, @last_sentinel.prev
-    @last_sentinel.prev = link
+    last_link = @last_sentinel.prev
+    unless last_link.nil?
+      last_link.next, link.prev = link, @last_sentinel.prev
+    else
+      @begin_sentinel.next = link
+    end
+      @last_sentinel.prev = link
 
     self
   end
 
-
   def unshift(val)
     link = Link.new(val)
-    @begin_sentinel.next.prev, link.next = link, @begin_sentinel.next
+    first_link = @begin_sentinel.next
+    unless first_link.nil?
+      @begin_sentinel.next.prev, link.next = link, @begin_sentinel.next
+    else
+      @last_sentinel.prev = link
+    end
     @begin_sentinel.next = link
 
     self
@@ -34,6 +43,7 @@ class LinkedList
 
   def shift
     first_link = @begin_sentinel.next
+    return nil if first_link.nil?
     val = first_link.val
     @begin_sentinel = first_link.next
 
---
+++ Ruby/spec/doubly_linked_list_spec.rb
@@ -0,0 +1,74 @@
+require 'debugger'
+require "doubly_linked_list"
+
+RSpec.describe LinkedList do
+  before :each do
+    @linked_list = LinkedList.new
+  end
+
+  describe "initialize" do
+    it "creates a Linked List" do
+      expect(@linked_list.class).to be LinkedList
+    end
+
+    it "initializes with 2 sentinel links" do
+      expect(@linked_list.begin_sentinel.class).to be Link
+      expect(@linked_list.last_sentinel.class).to be Link
+    end
+  end
+
+  describe "#push" do
+    it "returns self" do
+      return_val = @linked_list.push(5)
+      expect(return_val).to be @linked_list
+    end
+
+    it "pushes a new link into the list" do
+      @linked_list.push(1)
+      val = @linked_list.last_sentinel.prev.val
+      expect(val).to be 1
+    end
+  end
+
+  describe "#unshift" do
+    it "returns self" do
+      return_val = @linked_list.unshift(5)
+      expect(return_val).to be @linked_list
+    end
+
+    it "unshifts a new link into the list" do
+      @linked_list.unshift(2)
+      first_link = @linked_list.begin_sentinel.next
+      expect(first_link.val).to be 2
+    end
+  end
+
+  context "pop and shift" do
+    before :each do
+      @linked_list.push(1)
+      @linked_list.push(2)
+      @linked_list.push(3)
+      @linked_list.push(4)
+    end
+
+    describe "#pop" do
+      it "returns the last value in the list" do
+        pop1 = @linked_list.pop
+        pop2 = @linked_list.pop
+        expect(pop1).to eq 4
+        expect(pop2).to eq 3
+      end
+    end
+
+    describe "#unshift" do
+      it "returns the first value in the list" do
+        shift1 = @linked_list.shift
+        shift2 = @linked_list.shift
+        expect(shift1).to eq 1
+        debugger;
+        expect(shift2).to eq 2
+      end
+    end
+  end
+
+end
