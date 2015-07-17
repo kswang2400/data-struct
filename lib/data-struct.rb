9f0153a0b65b07605e209ca063882791ca1d686e
Kevin <kevinwang2400@gmail.com>
2015-07-17 12:55:35 -0700

    green specs

--- lib/data-struct.rb
+++ lib/data-struct.rb
@@ -2,8 +2,8 @@
 require_relative "doubly_linked_list"                            
 require_relative "dynamic_array"
 require_relative "max_stack"
-require_relative "min_max_queue"
 require_relative "min_max_stack"
+require_relative "min_max_queue"
 require_relative "singly_linked_list"
 require_relative "stack_queue"
 
--- lib/singly_linked_list.rb
+++ lib/singly_linked_list.rb
@@ -24,7 +24,7 @@ class SingleLinkedList
 
   def shift
     first_link = @sentinel.next
-    raise "cannot shift from empty list" if prev.nil?
+    raise "cannot shift from empty list" if first_link.nil?
     val = first_link.val
     @sentinel.next = first_link.next
 
