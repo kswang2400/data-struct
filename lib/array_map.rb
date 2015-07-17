0ed13e33af4f59d55e24e6e9f6105ac4bb4dc1ba
Daniel Ng <dvng13@gmail.com>
2015-07-17 15:34:42 -0700

    add array map and hash

---
+++ lib/array_map.rb
@@ -0,0 +1,38 @@
+class ArrayMap
+  attr_accessor :store
+
+  def initialize
+    @store = []
+  end
+
+
+# O(n)
+  def insert(key, value)
+    @store.push([key, value]) unless find(key)
+  end
+
+# O(n)
+  def get(key)
+    find(key).last
+  end
+
+# O(n)
+  def find(key)
+    @store.each do |k, v|
+      return [k, v] if k == key
+    end
+    nil
+  end
+# O(n)
+  def remove(key)
+    @store.each_with_index do |pair, idx|
+       if key == pair.first
+         @store.delete_at(idx)
+         return true
+       end
+    end
+
+    false
+  end
+
+end
