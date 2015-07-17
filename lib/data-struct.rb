ae6e6a9bb7474eb677c32ca1d284c41b720651a9
Kevin <kevinwang2400@gmail.com>
2015-07-17 12:29:41 -0700

    fixed gem

--- lib/data-struct.rb
+++ lib/data-struct.rb
@@ -1,10 +1,13 @@
 
-module DataStruct
-end
-
-# require_relative "doubly_linked_list"
+require_relative "doubly_linked_list"                            
 require_relative "dynamic_array"
 require_relative "max_stack"
+require_relative "min_max_queue"
 require_relative "min_max_stack"
 require_relative "singly_linked_list"
 require_relative "stack_queue"
+
+module DataStruct
+  class DynamicArray < DynamicArray
+  end
+end
