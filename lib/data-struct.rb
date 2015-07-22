e4c9fc678694c3b94c1270b28ee6eb91e6498ffe
Kevin <kevinwang2400@gmail.com>
2015-07-22 08:21:43 -0700

    preparing for new gem update

--- lib/data-struct.rb
+++ lib/data-struct.rb
@@ -1,17 +1,18 @@
 
-# require_relative "doubly_linked_list"                         
+require_relative "binary_search_tree"
 require_relative "dynamic_array"
+# require_relative "doubly_linked_list"                         
 require_relative "max_stack"
 require_relative "min_max_stack"
 # require_relative "min_max_queue"
 require_relative "singly_linked_list"
 # require_relative "stack_queue"
-require_relative "binary_search_tree"
 
 module DataStruct
+  class BinarySearchTree < BinarySearchTree; end
   class DynamicArray < DynamicArray; end
   class MaxStack < MaxStack; end
   class MinMaxStack < MinMaxStack; end
   class SinglyLinkedList < SinglyLinkedList; end
-  class BinarySearchTree < BinarySearchTree; end
 end
+
